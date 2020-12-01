53be6f9d8baa074d3224e20f3fe4ac84673c0bb7
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-01 13:57:45 +0100

    Fix problems with subtitle to img

--- webb20-js-lesson-2-react/src/App.js
+++ webb20-js-lesson-2-react/src/App.js
@@ -8,29 +8,31 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
+      <header className="App-header">
 
-        <Image description='It is a logo' image={logo} title='React Logo'/>
-        <Image description='It is a cat' image='http://placekitten.com/200/300' title='Cat'/>
-        <Image description='This is another cat' image='http://placekitten.com/300/400' title='Cat 2'/>
-
+      <Image description="This is the React logotype" image={logo} title="React Logo"/>
+        <Image description="This is a cat" image="http://placekitten.com/200/300" title="Cat"/>
+        <Image description="This is another cat" image="http://placekitten.com/300/400" title="Cat 2"/>
+        
         <Paragraph title="Information">
           Edit <code>src/App.js</code> and save to reload.
         </Paragraph>
-        <Paragraph title="Whats happening?">
-          Im curently learning React.
-        </Paragraph>
 
+        <Paragraph title="Whats happening?">
+          Im currently learning React
+        </Paragraph>
+        
         <Paragraph>
-          <ul><li>React</li>
+          <ul>
+            <li>React</li>
             <li>Props</li>
             <li>Deconstructing</li>
             <li>Props Children</li>
           </ul>
         </Paragraph>
 
-        <Link url='https://reactjs.org' text='Learn React' />
-        <Link url='https://svt.se' text='Go to SVT' />
+        <Link url="https://reactjs.org" text="Learn React" />
+        <Link url="https://svt.se" text="Go to SVT" />
       </header>
     </div>
   );
--- webb20-js-lesson-2-react/src/components/Image.jsx
+++ webb20-js-lesson-2-react/src/components/Image.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import Link from './Link'
 
-
 export default function Image(props) {
   let counter = 0
 
   function onClickHandler() {
-    /* alert(props.title)  - TEST */
-    counter = counter + 1 // or counter+=1
+    counter += 1
     console.log(counter)
   }
 
+  console.log(props)
+
   return (
-    <div>
+    <>
       <Link url={props.image} text={props.title} />
-      <img onClick={(onClickHandler)} src={props.image} alt='logo' />
-      <strong>{props.description}</strong>
-      <p>{counter}</p>
-    </div>
+      <img onClick={onClickHandler} src={props.image} alt="logo" />
+      {props.description ? <strong>{props.description}</strong> : "No description" }
+      {counter}
+    </>
   )
 }
+/*
+    1. Returnera JSX
+    2. Endast returnera ett element (men som i sin tur kan ha oändligt många child-komponenter)
+
+    Extrapoäng:
+    * Props får inte ändras (mutera)
+*/
